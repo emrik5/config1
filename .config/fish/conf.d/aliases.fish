f4a25037d742d9ac1614217076e21cadf5adc30e
Parrot <parrot@localhost.localdomain>
2024-11-14 18:42:23 +0100

    parrot fish config

---
+++ .config/fish/conf.d/aliases.fish
@@ -0,0 +1,29 @@
+# general aliases
+alias cls='clear'
+alias quit='exit'
+alias md='mkdir'
+alias which='type'
+alias nul='/dev/null'
+alias shutd='sudo shutdown 0'
+alias m='man'
+
+# apt aliases
+alias install='sudo apt install'
+alias uninstall='sudo apt remove'
+alias aptup='sudo apt update'
+alias fullupdate='sudo apt update && sudo apt full-upgrade'
+
+# ls aliases
+alias ll='exa -lagh --git'
+alias l='exa -aF'
+
+# cd aliases
+alias ..='cd ..'
+alias cdc='cd ~; clear'
+
+# Code
+alias c='code -n .'
+alias cr='code -r .'
+
+# Net
+alias trace='traceroute'
---
+++ .config/fish/conf.d/eh_aliases.fish
@@ -0,0 +1,2 @@
+alias vpnc="sudo ~/EH-VPN/vpn-connect.sh"
+alias vpnd="sudo ~/EH-VPN/vpn-connect.sh --disconnect"
