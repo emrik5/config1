f06003502a799fd08b0aed949bca2129729e80e3
e5 <emrikulwan@gmail.com>
2025-03-03 10:06:29 +0100

    Improve func que, now does whatis, which, and apropos

--- .config/fish/functions/que.fish
+++ .config/fish/functions/que.fish
@@ -1,6 +1,10 @@
 function que
+	set -f whatfail ""
  	if whatis "$argv" &>/dev/null
 		whatis "$argv"
+	else if apropos "$argv" &>/dev/null
+		echo "Found:"
+		apropos "$argv"
 	else
 		set -f whatfail 1
  	end
