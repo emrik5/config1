0adf392513f00e6de2aac8b39e2d2f99e3c9c4f7
Arch <emrikulwan@gmail.com>
2025-04-12 23:35:22 +0200

    arch fixes: sway, foot, tmux, etc.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set PATH $PATH ~/.cargo/bin
-set VISUAL vim
+set VISUAL nvim
 set __fish_git_prompt_showupstream informative
 set __fish_git_prompt_show_informative_status 1
