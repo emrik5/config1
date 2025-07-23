6b344eaafece9f47e180c0050e04e8d68571e5bc
Arch <emrikulwan@gmail.com>
2025-07-23 14:13:39 +0200

    Add sway shortcut to SIGKILL focused + volume indicator work

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,4 +1,12 @@
 date=$(date +'%Y-%m-%d %X')
-media=$(playerctl -f "{{artist}} - {{title}}" metadata)
+if media=$(playerctl -f "{{artist}} - {{title}}" metadata); then
+	media="${media}  |  "
+fi
 bat=$(cat /sys/class/power_supply/BAT0/capacity)
-echo "$media  |  $date | $bat%"
+
+#volume=$(amixer -D pulse get Master | awk -F 'Left:|[][]' 'BEGIN {RS=""}{ print $3 }')
+#if [$volume != $AUDIO_VOLUME]; then
+#	playerctl pause
+#	export AUDIO_VOLUME=$volume
+#fi
+echo "$media$date | $bat%"
