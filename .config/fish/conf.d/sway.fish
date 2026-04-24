bd38c010efede13a2251ca9d1141868a02b16ad5
Arch <emrikulwan@gmail.com>
2026-04-24 16:45:58 +0200

    add sway logging to a file when on tty1

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -1,6 +1,6 @@
 if status is-login
     if test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" = 1 -a (tty) = "/dev/tty1"
-		exec sway --unsupported-gpu 
+		exec sway --unsupported-gpu &>~/.local/state/sway.log
 	end
 end
 
